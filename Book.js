6752aa21850412d492488ba13ae43f2dbf2b37c4
Natalia Titova <bnmlucky@gmail.com>
2020-03-05 15:07:06 -0500

    added more properties to the Book class to track whether the book is checked out or not, due date and patron

--- Book.js
+++ Book.js
@@ -1,7 +1,10 @@
 class Book {
-    constructor(title, author, isbn) {
+    constructor(title, author, isbn, out, dueDate, patron) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.out = false;
+        this.dueDate = null;
+        this.patron = null;
     }
 }
